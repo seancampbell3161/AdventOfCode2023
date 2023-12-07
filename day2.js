5b1a01fc18d533b49064f5546940d51e831f0adb
Sean Campbell <sean.campbell3161@gmail.com>
2023-12-06 20:30:55 -0600

    finish solution 2

--- day2.js
+++ day2.js
@@ -1,9 +1,9 @@
 const input = 
 `Game 1: 4 red, 1 green, 15 blue; 6 green, 2 red, 10 blue; 7 blue, 6 green, 4 red; 12 blue, 10 green, 3 red
 Game 2: 3 green, 18 blue; 14 green, 4 red, 2 blue; 3 red, 14 green, 15 blue
-Game 3: 12 green, 2 blue; 9 green; 1 red, 11 blue, 4 green
-Game 4: 4 blue, 8 green, 5 red; 6 red, 7 blue, 9 green; 2 green, 2 red, 2 blue; 2 green, 6 blue, 9 red; 10 red, 9 green
-Game 5: 12 red, 1 green, 7 blue; 13 red, 16 blue; 16 blue, 10 red; 4 blue; 16 blue, 7 red; 1 blue, 7 red`;
+Game 3: 12 green, 2 blue; 9 green; 1 red, 11 blue, 4 green`;
+
+// ---------- Solution 1 ----------
 
 const colorLimits = new Map();
 colorLimits.set('red', 12);
@@ -37,4 +37,40 @@ function getSumOfIds() {
     }, 0);
 }
 
-console.log(getSumOfIds());
+// console.log(getSumOfIds());
+
+// ---------- Solution 2 ----------
+
+function findSumOfPowers() {
+    const lines = input.split('\n');
+    const powers = lines.map(line => {
+        
+        let highestRed = 0;
+        let highestGreen = 0;
+        let highestBlue = 0;
+        const drawings = line.split(':').pop().split(';');
+        
+        drawings.forEach(drawing => {       
+            const coloredCubes = drawing.split(', ');    
+            coloredCubes.forEach((numAndColor) => {     
+                numAndColor = numAndColor.trim();
+                numColorArray = numAndColor.split(' ');
+            
+                if (numColorArray[1] === 'red' && Number(numColorArray[0]) > highestRed) {
+                    highestRed = Number(numColorArray[0]);
+                } else if (numColorArray[1] === 'green' && Number(numColorArray[0]) > highestGreen) {
+                    highestGreen = Number(numColorArray[0]);
+                } else if (numColorArray[1] === 'blue' && Number(numColorArray[0]) > highestBlue) {
+                    highestBlue = Number(numColorArray[0])
+                }
+            });
+        });
+
+        return highestRed * highestGreen * highestBlue;
+    });
+    return powers.reduce((acc, cur) => {
+        return acc += cur;
+    }, 0);
+}
+
+console.log(findSumOfPowers());
